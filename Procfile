ecb688c793c6e8d3192cc18822e3c94fedb5eafe
Jesus Briones <leojesusbriones2@gmail.com>
2023-11-01 14:31:17 -0500

    form1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main.wsgi
+web: python manage.py collectstatic && gunicorn main.wsgi
--- staticfiles/main.js
+++
@@ -1,9 +0,0 @@
-const $counter = document.querySelector('.counter')
-let i = 0;
-
-function counter() {
-  i++
-  $counter.textContent = i;
-}
-
-document.querySelector('.button').addEventListener("click", counter)
