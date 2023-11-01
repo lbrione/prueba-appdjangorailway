457147d81d789630d08fa02c9788fe7cca3b75fb
Jesus Briones <leojesusbriones2@gmail.com>
2023-11-01 14:12:23 -0500

    appdjango2

--- main/settings.py
+++ main/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
